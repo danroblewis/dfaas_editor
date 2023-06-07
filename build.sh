7cd26ded8b86079768fb61156c6a015a1825bcab
Daniel Lewis <daniel.robert.lewis@gmail.com>
2023-06-07 03:17:03 +0000

    i dont know what i did

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 docker build -t danroblewis/dfaas-webide:latest .
-docker run --env-file .env -it danroblewis/dfaas-webide:latest
+docker run -p 3001:3001 --env-file .env -it danroblewis/dfaas-webide:latest
